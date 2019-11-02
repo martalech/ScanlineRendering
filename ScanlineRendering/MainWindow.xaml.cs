3faa2d507db361f8b07b53494fa473298c319e92
martalech <martalech98@gmail.com>
2019-11-02 10:03:40 +0100

    wolne i nie ma ruszajacego swiatla i metody wypelniania

--- ScanlineRendering/MainWindow.xaml.cs
+++ ScanlineRendering/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.Win32;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Diagnostics;
@@ -16,6 +17,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Media.Media3D;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Windows.Threading;
 
 public class VisualHost: UIElement
 {
@@ -42,6 +44,7 @@ namespace ScanlineRendering
         private List<EdgeET>[] ET = null;
         private List<EdgeET> AET = null;
         private List<Triangle> triangles = new List<Triangle>();
+        private int n1, m1, ncount, mcount;
         private Vertex[,] vertices;
 
         public bool KMMode { get; set; } = false;
@@ -50,22 +53,25 @@ namespace ScanlineRendering
         public bool IoMode { get; set; } = false;
         public bool LMode { get; set; } = false;
 
+        Vector3D LVector = new Vector3D(0, 0, 1);
+
+        private byte[] NBitmap, ColBitmap;
+        private int nBitmapStride, colBitmapStride;
         private double Angle(Vector3D v1, Vector3D v2)
         {
             return Vector3D.DotProduct(v1, v2);
         }
 
-        private Vector3D I(double kd, double ks, double m, Color c)
+        private Vector3D I(double kd, double ks, double m, Color c, Vector3D N)
         {
-            Vector3D N, V, L, IO, IL, R;
+            Vector3D V, IO, IL, R;
             V = new Vector3D(0, 0, 1);
-            L = new Vector3D(0, 0, 1);
+            //L = new Vector3D(0, 0, 1);
             IL = new Vector3D(1, 1, 1);
             IO = new Vector3D(c.R, c.G, c.B);
-            N = new Vector3D(0, 0, 1);
-            R = 2 * N - L;
-            var lol = Vector3D.AngleBetween(N, L);
-            double angle1 = Math.Cos(Angle(N, L));
+            R = 2 * N - LVector;
+            var lol = Vector3D.AngleBetween(N, LVector);
+            double angle1 = Math.Cos(Angle(N, LVector));
             double angle2 = Math.Pow(Math.Cos(Angle(V, R)), m);
             return new Vector3D((kd * IL.X * IO.X * angle1) + (ks * IL.X * IO.X * angle2),
                 (kd * IL.Y * IO.Y * angle1) + (ks * IL.Y * IO.Y * angle2),
@@ -76,6 +82,21 @@ namespace ScanlineRendering
         {
             InitializeComponent();
             trianglesInfo = new TrianglesInfo("70", "90");
+            DispatcherTimer dispatcherTimer = new DispatcherTimer();
+            dispatcherTimer.Tick += DispatcherTimer_Tick;
+            dispatcherTimer.Interval = new TimeSpan(0, 0, 12);
+            dispatcherTimer.Start();
+        }
+
+        private void DispatcherTimer_Tick(object sender, EventArgs e)
+        {
+            if (LMode)
+            {
+                double angle = Math.PI / 180 * 20;
+                LVector.X = LVector.X * Math.Cos(angle) - LVector.Y * Math.Sin(angle);
+                LVector.Y = LVector.X * Math.Sin(angle) + LVector.Y * Math.Cos(angle);
+                RepaintTriangles();
+            }
         }
 
         private void OnCanvasMouseDown(object sender, MouseButtonEventArgs e)
@@ -143,8 +164,12 @@ namespace ScanlineRendering
             int.TryParse(trianglesInfo.M, out m);
             double width = Board.ActualWidth + Board.Margin.Left + Board.Margin.Right;
             double height = Board.ActualHeight + Board.Margin.Top + Board.Margin.Bottom;
-            var widthCount = (int)Math.Round((Board.ActualWidth + Board.Margin.Left * 2) / n);
-            var heightCount = (int)Math.Round((Board.ActualHeight + Board.Margin.Top * 2) / m);
+            var widthCount = (int)Math.Round(Board.ActualWidth / n);
+            var heightCount = (int)Math.Round(Board.ActualHeight / m);
+            n1 = n;
+            m1 = m;
+            ncount = widthCount;
+            mcount = heightCount;
             //PaintTriangles();
             if (clear)
             {
@@ -248,7 +273,6 @@ namespace ScanlineRendering
                         X2 = edge.To.X,
                         Y2 = edge.To.Y
                     };
-                    line.MouseUp += OnCanvasMouseUp;
                     Board.Children.Add(line);
                 }
             }
@@ -291,12 +315,9 @@ namespace ScanlineRendering
             DrawingVisual drawingVisual = new DrawingVisual();
             DrawingContext drawingContext = drawingVisual.RenderOpen();
             double kd, ks, m;
-            Color c;
-            if (IoMode)
-            {
-                c = Colors.Red;
-            }
-            else
+            Color c = Colors.Blue;
+            Vector3D N = new Vector3D(0, 0, 1);
+            if (!IoMode)
             {
                 if (ColPicker.SelectedColor.HasValue)
                     c = ColPicker.SelectedColor.Value;
@@ -337,9 +358,27 @@ namespace ScanlineRendering
                         double x2 = AET[k + 1].xmin;
                         for (double x = x1; x < x2; x++)
                         {
-                            Vector3D vector = I(kd, ks, m, c);
-                            Color col = Color.FromRgb((byte)vector.X, (byte)vector.Y, (byte)vector.Z);
-                            var rec = new Rect(new Point(x - 1, indmin - 1), new Size(2, 2));
+                            Color col;
+                            if (IoMode)
+                            {
+                                int index = (int)Math.Round(x) * 4 + indmin * colBitmapStride;
+                                c = Color.FromRgb(ColBitmap[index + 2], ColBitmap[index + 1], ColBitmap[index]);
+                            }
+                            if (NMode)
+                            {
+                                int index = (int)Math.Round(x) * 4 + indmin * nBitmapStride;
+                                Vector3D v3d = new Vector3D(NBitmap[index + 2], NBitmap[index + 1], NBitmap[index]);
+                                if(v3d.X != 128)
+                                {
+                                    Debug.WriteLine("elo");
+                                }
+                                N = new Vector3D((2 * (double)NBitmap[index + 2] - 256) / 255, (2 * (double)NBitmap[index + 1] - 256) / 255,
+                                    (2 * (double)NBitmap[index] - 255) / 255);
+                                //N.Normalize();
+                            }
+                            Vector3D vector = I(kd, ks, m, c, N);
+                            col = Color.FromRgb((byte)vector.X, (byte)vector.Y, (byte)vector.Z);
+                            var rec = new Rect(new Point(x - 0.5, indmin - 0.5), new Size(2, 2));
                             drawingContext.DrawRectangle(new SolidColorBrush(col), null, rec);
                         }
                     }
@@ -361,8 +400,19 @@ namespace ScanlineRendering
             }
             drawingContext.Close();
             var vs = new VisualHost { Visual = drawingVisual };
-            vs.MouseUp += OnCanvasMouseUp;
             Board.Children.Add(vs);
+        }
+
+        private void OnLModeClick(object sender, RoutedEventArgs e)
+        {
+            LVector = new Vector3D(1, 1, 1);
+            RepaintTriangles();
+        }
+
+        private void OnNoLModeClick(object sender, RoutedEventArgs e)
+        {
+            LVector = new Vector3D(0, 0, 1);
+            RepaintTriangles();
         }
 
         private bool IsArrayEmpty(List<EdgeET>[] array)
@@ -382,9 +432,51 @@ namespace ScanlineRendering
             return false;
         }
 
+        private void OnLoadNButtonClick(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "Images (*.png;*.jpeg;*.bmp;*.jpg)|*.png;*.jpeg;*.bmp;*.jpg";
+            if (openFileDialog.ShowDialog() == true)
+            {
+                string filepath = openFileDialog.FileName;
+                if (filepath != null && filepath.Length > 0)
+                {
+                    BitmapImage nBitmap = new BitmapImage(new Uri(filepath));
+                    double scaleX = Board.ActualWidth / nBitmap.PixelWidth;
+                    double scaleY = Board.ActualHeight / nBitmap.PixelHeight;
+                    var nBitmap2 = new TransformedBitmap(nBitmap, new ScaleTransform(scaleX, scaleY));
+                    nBitmapStride = ((nBitmap2.PixelWidth * nBitmap2.Format.BitsPerPixel + 7) / 8);
+                    int nBitmapSize = nBitmapStride * nBitmap2.PixelHeight;
+                    NBitmap = new byte[nBitmapSize];
+                    nBitmap2.CopyPixels(NBitmap, nBitmapStride, 0);
+                }
+            }
+        }
+
         private void OnApplyChangesClick(object sender, RoutedEventArgs e)
         {
             RepaintTriangles(true);
+        }
+
+        private void OnLoadColorButtonClick(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "Images (*.png;*.jpeg;*.bmp;*.jpg)|*.png;*.jpeg;*.bmp;*.jpg";
+            if (openFileDialog.ShowDialog() == true)
+            {
+                string filepath = openFileDialog.FileName;
+                if (filepath != null && filepath.Length > 0)
+                {
+                    BitmapImage colBitmap = new BitmapImage(new Uri(filepath));
+                    double scaleX = Board.ActualWidth / colBitmap.PixelWidth;
+                    double scaleY = Board.ActualHeight / colBitmap.PixelHeight;
+                    var colBitmap2 = new TransformedBitmap(colBitmap, new ScaleTransform(scaleX, scaleY));
+                    colBitmapStride = ((colBitmap2.PixelWidth * colBitmap2.Format.BitsPerPixel + 7) / 8);
+                    int colBitmapSize = colBitmapStride * colBitmap2.PixelHeight;
+                    ColBitmap = new byte[colBitmapSize];
+                    colBitmap2.CopyPixels(ColBitmap, colBitmapStride, 0);
+                }
+            }
         }
     }
 }
