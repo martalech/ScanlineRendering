23fe5dafd4b02d51dcdf3aabdee3fd036e520e0f
martalech <martalech98@gmail.com>
2019-11-04 20:58:39 +0100

    v6.0

--- ScanlineRendering/MainWindow.xaml.cs
+++ ScanlineRendering/MainWindow.xaml.cs
@@ -13,7 +13,6 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Media.Media3D;
-using System.Windows.Shapes;
 using System.Windows.Threading;
 
 namespace ScanlineRendering
